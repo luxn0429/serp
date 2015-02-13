ad08d72c5c792967ba27b013540f7137ecc6011f
luxn0429 <luxn0429@gmail.com>
2015-02-13 16:43:29 +0800

    init project

---
+++ crm/net/loyin/ctrl/crm/indexCtrl.java
@@ -0,0 +1,10 @@
+package net.loyin.ctrl.crm;
+
+import net.loyin.ctrl.base.BaseController;
+import net.loyin.jfinal.anatation.RouteBind;
+
+@SuppressWarnings("rawtypes")
+@RouteBind(path = "/crm")
+public class indexCtrl extends BaseController {
+
+}
---
+++ sso/net/loyin/ctrl/sso/MessageCtrl.java
@@ -0,0 +1,13 @@
+package net.loyin.ctrl.sso;
+
+import net.loyin.ctrl.base.AdminBaseController;
+import net.loyin.jfinal.anatation.RouteBind;
+/**
+ * 消息
+ * @author 龙影
+ */
+@RouteBind(path="message")
+@SuppressWarnings("rawtypes")
+public class MessageCtrl extends AdminBaseController {
+
+}
